240d888c4f48b5169aeae5b290c7c504b339ab2d
Wojciech Zajączkowski <zajaczkowski.wojtek003@gmail.com>
2023-03-02 16:35:54 +0100

    auuu pobeirajcie
    łelkom

--- website/auth.py
+++ website/auth.py
@@ -87,55 +87,6 @@ def you():
 
     return render_template("you.html")
 
-
-
-
-
-def number_of_similars(gender, age, height, weight, skin_colour):
-    age = int(age)
-    height = int(height)
-    weight = int(weight)
-
-    users = User.query.all()
-    counter = int(0)
-    for user in users:
-        if (gender == gender and difference(age, user.age, 2) and difference(height, user.height, 3) and
-                difference(weight, user.weight, 2) and skin_colour == user.skin_colour):
-            counter += 1
-
-    return counter
-
-
-def number_of_identical(gender, age, height, weight, silhouette, hair_colour, facial_hair, glasses, skin_colour, eye_colour):
-
-    age = int(age)
-    height = int(height)
-    weight = int(weight)
-
-    number_of_identic = User.query.filter_by(
-                                gender=gender, age=age, height=height, weight=weight, silhouette=silhouette,
-                                hair_colour=hair_colour, facial_hair=facial_hair, glasses=glasses,
-                                skin_colour=skin_colour, eye_colour=eye_colour
-                                ).count()
-
-    return number_of_identic
-
-
-def difference(a, b, max_difference):
-    a = int(a)
-    max_difference = int(max_difference)
-
-    if abs(a-b) <= max_difference:
-        return True
-
-    return False
-
-
-def print_whole_table():
-    users = User.query.all()
-    for user in users:
-        print(user.id, user.ipaddress, user.date ,user.gender, user.age, user.height, user.weight, user.silhouette, user.hair_colour, user.facial_hair, user.glasses, user.skin_colour, user.eye_colour)
-
 @auth.route('/statistics', methods=['GET', 'POST'])
 def statistics():
     # hair statistics
